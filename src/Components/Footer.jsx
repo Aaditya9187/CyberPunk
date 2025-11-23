d20aae0073ef7889f4b131e8ef065764a23068a3
Swastik <sharmaspeedx29@gmail.com>
2025-11-24 00:09:58 +0530

    ui update

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -12,7 +12,7 @@ const Footer = () => {
     <footer className='w-screen bg-violet-300 py-4 text-black'>
       <div className='container mx-auto flex flex-col items-center justify-between gap-4 px-4 md:flex-row'>
         <p className='text-center text-sm md:text-left md:flex-1'>
-          &copy; Cyberpunk Edgerunners. All rights reserved
+          &copy; Cyberpunk Edgerunners
         </p>
         
         <div className='flex justify-center gap-4 md:flex-1 md:justify-center'>
@@ -33,7 +33,7 @@ const Footer = () => {
           href='#privacy-policy' 
           className='text-center text-sm hover:underline md:text-right md:flex-1'
         >
-          Privacy Policy
+          The Night City
         </a>
       </div>
     </footer>
