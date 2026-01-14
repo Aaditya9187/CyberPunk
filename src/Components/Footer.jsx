52b216fa3b6ffa4caddb767ee9fe9171510cdaa7
Swastik <sharmaspeedx29@gmail.com>
2026-01-14 19:08:35 +0530

    updated links

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -11,7 +11,7 @@ const Footer = () => {
   return (
     <footer className='w-screen bg-violet-300 py-4 text-black'>
       <div className='container mx-auto flex flex-col items-center justify-between gap-4 px-4 md:flex-row'>
-        <p className='text-center text-sm md:text-left md:flex-1'>
+        <p className='text-center hover:underline text-sm md:text-left md:flex-1'>
           &copy; Cyberpunk Edgerunners
         </p>
         
@@ -29,12 +29,9 @@ const Footer = () => {
           ))}
         </div>
 
-        <a 
-          href='#privacy-policy' 
-          className='text-center text-sm hover:underline md:text-right md:flex-1'
-        >
+        <p className='text-center text-sm hover:underline md:text-right md:flex-1'>
           The Night City
-        </a>
+        </p>
       </div>
     </footer>
   )
